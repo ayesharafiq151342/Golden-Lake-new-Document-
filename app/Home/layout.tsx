d2b1ff191e3986823524a4f2b04ff969bd6780f5
Ayesha Rafiq <ayesharafiq151342@gmail.com>
2025-02-20 20:44:40 +0500

    second

--- app/Home/layout.tsx
+++ app/Home/layout.tsx
@@ -1,6 +1,5 @@
 // app/layout.tsx
 import React from 'react';
-import './globals.css'; // Global styles
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    appDir: true
-  }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
